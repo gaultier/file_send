e96cb93a8261874242235872bfdca17c9a0e24b2
Philippe Gaultier <philippe.gaultier@ory.sh>
2026-09-25 20:49:03 +0200

    wip win32

---
+++ win32.c
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+__attribute__((warn_unused_result)) static IO io_platform_make(void) {
+  return (IO){
+      0 // TODO
+  };
+}
